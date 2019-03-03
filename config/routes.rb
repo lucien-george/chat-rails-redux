a7dccb3b96631d2b00d3f0a814291710fea5d114
Lucien George <luciengeorge95@gmail.com>
2019-03-03 17:13:36 +0000

    first commit. working app

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :channels, only: [] do
+        resources :messages, only: [ :index, :create ]
+      end
+    end
+  end
+
+  resources :channels, only: [ :show ]
+  root to: 'channels#show'
+
+  mount ActionCable.server => "/cable"
+end
